c2d3e75f7ad17caa152ed019198fa95db2a14d24
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2022-08-03 21:15:52 -0300

    Adicionando basico de tutorial interativo

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex = 0;
+
+    void Update(){
+
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i == popUpIndex){
+                popUps[popUpIndex].SetActive(true);
+            }else{
+                popUps[i].SetActive(false);
+            }
+        }
+
+        if(popUpIndex == 0){
+            if(Input.GetMouseButtonDown(0)){
+                popUpIndex++;
+            }
+        }else if (popUpIndex == 1){
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)){
+                popUpIndex++;
+            }
+        }
+    }
+}
